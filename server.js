fe8cfd1b519f2aa2d9a3d1d8f96f11a7f8426f08
Gisellabella <gisella@gmail.com>
2016-12-22 20:25:14 -0700

    pushing tweaks to db api routes

--- server.js
+++ server.js
@@ -7,40 +7,44 @@ const port    = process.env.PORT || 3000;
 
 
 // var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-var mongoose = require('mongoose');
-
+var bodyParser 	= require('body-parser');
+var jsonParser 	= bodyParser.json();
+var mongoose 	= require('mongoose');
 
 var db = require('./models');
 
 
 
+// --------------API ROUTES
+
+// (#index) route
 app.get('/', function (req, res) {
 	db.Card.find(function(err, cards){
-		if (err) {console.log(err);
-		}
-		
-		res.json(cards);
+				if (err) {
+				return console.log(err);
+				}
+	res.json(cards);
+	});
 });
+// (#show) route
+app.get('/cards/:id', function(req, res) {
+	db.Card.findOne({_id: req.params.id},
+	function (err, card) {
+	res.json(card);
+	});
 });
 
-
-app.get('/api/cards/:id', function(req, res) {
-// db.Card.findOne({_id: req.params.id }
-// function(err, card) {
-res.json(card);
-});
-
-// });
-
-
-
+// (#create) route
 app.post('/', jsonParser, function (req, res){
-	// if (!req.body) return res.sendStatus(400);
-	questionList.push(req.body);
-	res.json(req.body);
-});
+	var newCard = new db.Card (req.body);
+	newCard.save (function (err, card){
+          if (err) { 
+            return console.log("Got a get card posting error: " + err);
+          } 
+         res.json(card);
+ 	});
+ });
+
 
 
 app.use(express.static(__dirname + '/public'));
